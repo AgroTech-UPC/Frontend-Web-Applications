c359b673b4ab9b2902b98c15e27f1747aaf67c35
Piero Delgado <112528610+PieroD04@users.noreply.github.com>
2024-05-01 18:46:40 -0500

    feat: added routes for my animals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'animales', component: CageListComponent },
+  { path: 'animales/:id', component: AnimalListComponent },
+  { path: 'animales/edit/:id', component: CageEditorComponent },
+  { path: 'animales/:cageid/informacion/:id', component: AnimalInformationComponent },
+];
