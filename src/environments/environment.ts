54bbef73ca395024e7055357ac0dc407435f550d
Nadia <nad21lc@gmail.com>
2024-05-03 17:09:25 -0500

    feat: Added endpoint for the publication-view

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,19 @@
 export const environment = {
   production: false,
     // The base URL for the AgroConnectAPI
-    baseURL: 'http://localhost:3000'
+    baseURL: 'http://localhost:3000',
+
+    //urls for the different endpoints
+    userURL: 'users',
+    advisorURL: 'advisors',
+    breederURL: 'breeders',
+    resourceURL: 'resources',
+    appointmentURL: 'appointments',
+    expenseURL: 'expenses',
+    notificationURL: 'notifications',
+    publicationURL: 'publications',
+    animalURL: 'animals',
+    cageURL: 'cages',
+    reviewURL: 'reviews',
+    availableDateURL: 'available_dates'
 }
