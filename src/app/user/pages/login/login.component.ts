a3f6ce3ff2536a4fc273344fff496475ce4502f3
Piero Delgado <112528610+PieroD04@users.noreply.github.com>
2024-05-17 12:19:52 -0500

    feat: added login card and design

--- src/app/user/pages/login/login.component.ts
+++ src/app/user/pages/login/login.component.ts
@@ -1,28 +1,52 @@
-import { Component } from '@angular/core';
-import {MatButtonModule} from "@angular/material/button";
+import {Component, OnInit} from '@angular/core';
 import {UserApiService} from "../../services/user-api.service";
-import {Router} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    MatButtonModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInput,
+    ReactiveFormsModule,
+    MatButton,
+    RouterLink
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor(private userApiService: UserApiService, private router: Router) {}
+  constructor(private userApiService: UserApiService,
+              private router: Router,
+              private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email] ],
+      password: ['', Validators.required ]
+    });
+  }
+
+  login(){
+    console.log("hola");
+    this.userApiService.getAll().subscribe((data) => {
+      console.log(data);
+    });
+  }
 
   loginAsBreeder() {
-    this.userApiService.setIsBreeder(true);
     this.router.navigateByUrl('/criador/mi-granja');
   }
 
   loginAsAdvisor() {
-    this.userApiService.setIsBreeder(false);
     this.router.navigateByUrl('/asesor/clientes');
   }
 
