d18accccf2a4c82524bd7892964c1e3d9375a5e2
salvadoorssalinas <u20221b127@upc.edu.pe>
2024-05-18 12:33:35 -0500

    feat: fixed css, validators for password, date

--- src/app/user/components/register-advisor/register-advisor.component.ts
+++ src/app/user/components/register-advisor/register-advisor.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
-import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
+import {MatDatepickerModule, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
 import {MatError, MatFormField, MatHint, MatLabel, MatSuffix} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatNativeDateModule} from "@angular/material/core";
@@ -10,6 +10,7 @@ import {MatSelect} from "@angular/material/select";
 import {NgIf} from "@angular/common";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {DateAdapter, MAT_DATE_LOCALE} from "@angular/material/core";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'register-advisor',
@@ -20,7 +21,7 @@ import {DateAdapter, MAT_DATE_LOCALE} from "@angular/material/core";
         MatCardContent,
         MatCardHeader,
         MatCardTitle,
-        MatDatepicker,
+        MatDatepickerModule,
         MatDatepickerInput,
         MatDatepickerToggle,
         MatError,
@@ -38,7 +39,7 @@ import {DateAdapter, MAT_DATE_LOCALE} from "@angular/material/core";
   templateUrl: './register-advisor.component.html',
   styleUrl: './register-advisor.component.css',
   providers: [
-    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'}
+    {provide: MAT_DATE_LOCALE, useValue: 'es-PE'}
   ]
 })
 export class RegisterAdvisorComponent {
@@ -46,7 +47,7 @@ export class RegisterAdvisorComponent {
     {
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
       location: new FormControl('', [Validators.required]),
       birthDate: new FormControl(null, [Validators.required]),
       description: new FormControl('', [Validators.required]),
@@ -55,12 +56,22 @@ export class RegisterAdvisorComponent {
       photo: new FormControl('', [Validators.required])
     }
   );
+  minDate: Date;
+  maxDate: Date;
 
-  constructor(private dateAdapter: DateAdapter<Date>,) {
-    this.dateAdapter.setLocale('pt-BR');
+  constructor(private dateAdapter: DateAdapter<Date>,
+              private router: Router) {
+    this.dateAdapter.setLocale('es-PE');
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 90, 0, 1);
+    this.maxDate = new Date(currentYear - 18, 11, 31);
   }
 
   onSubmit() {
 
+  }
+
+  goBack() {
+    window.history.back();
   }
 }
--- src/app/user/components/register-breeder/register-breeder.component.ts
+++ src/app/user/components/register-breeder/register-breeder.component.ts
@@ -1,40 +1,41 @@
 import { Component } from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
-import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
+import {MatDatepickerModule, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
 import {MatError, MatFormField, MatHint, MatLabel, MatSuffix} from "@angular/material/form-field";
 import {MatNativeDateModule} from "@angular/material/core";
 import {MatInput} from "@angular/material/input";
 import {NgIf} from "@angular/common";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'register-breeder',
   standalone: true,
-    imports: [
-        MatButton,
-        MatCard,
-        MatCardContent,
-        MatCardHeader,
-        MatCardTitle,
-        MatDatepicker,
-        MatDatepickerInput,
-        MatDatepickerToggle,
-        MatError,
-        MatFormField,
-        MatHint,
-        MatNativeDateModule,
-        MatInput,
-        MatLabel,
-        MatSuffix,
-        NgIf,
-        ReactiveFormsModule
-    ],
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatCardTitle,
+    MatDatepickerModule,
+    MatDatepickerInput,
+    MatDatepickerToggle,
+    MatError,
+    MatFormField,
+    MatHint,
+    MatNativeDateModule,
+    MatInput,
+    MatLabel,
+    MatSuffix,
+    NgIf,
+    ReactiveFormsModule
+  ],
   templateUrl: './register-breeder.component.html',
   styleUrl: './register-breeder.component.css',
   providers: [
-    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'}
+    {provide: MAT_DATE_LOCALE, useValue: 'es-PE'}
   ]
 })
 export class RegisterBreederComponent {
@@ -42,18 +43,29 @@ export class RegisterBreederComponent {
     {
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
       location: new FormControl('', [Validators.required]),
       birthDate: new FormControl(null, [Validators.required]),
       description: new FormControl('', [Validators.required])
     }
   );
 
-  constructor(private dateAdapter: DateAdapter<Date>) {
-    this.dateAdapter.setLocale('pt-BR');
+  minDate: Date;
+  maxDate: Date;
+
+  constructor(private dateAdapter: DateAdapter<Date>,
+              private router: Router) {
+    this.dateAdapter.setLocale('es-PE');
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 90, 0, 1);
+    this.maxDate = new Date(currentYear - 18, 11, 31);
   }
 
   onSubmit() {
 
+  }
+
+  goBack() {
+    window.history.back();
   }
 }
