506168c4c15531d10bf5aaaafde6ad901110bf07
Andre Gabriel Valverde Mozo <u202218899@upc.edu.pe>
2024-06-22 03:52:21 -0500

    feat: Added button "Terminar Cita" in client-card.component"

--- src/app/appointment/components/client-card/client-card.component.ts
+++ src/app/appointment/components/client-card/client-card.component.ts
@@ -3,6 +3,8 @@ import {MatButton} from "@angular/material/button";
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 import { Router } from "@angular/router";
 import {Client} from "../../models/client.model";
+import {AppointmentApiService} from "../../services/appointment-api.service";
+import {Appointment} from "../../models/appointment.model";
 
 @Component({
   selector: 'client-card',
@@ -20,9 +22,19 @@ import {Client} from "../../models/client.model";
 export class ClientCardComponent {
   @Input() client!: Client;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private appointmentService: AppointmentApiService) { }
 
   goToAppointment(id: any) {
     this.router.navigateByUrl(`/asesor/clientes/${id}`);
+  }
+  finishAppointment() {
+    this.appointmentService.getOne(this.client.appointmentId).subscribe((appointment: Appointment) => {
+      appointment.status = "TERMINADO";
+      this.appointmentService.update(appointment.id, appointment).subscribe(() => {
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+          this.router.navigate([decodeURI(this.router.url)]);
+        });
+      });
+    });
   }
 }
